c5de369e84b9bf344bf2b48a7abdd8421c241de2
dengjun <412549415@qq.com>
2018-04-22 23:07:18 +0800

    EurekaAndRibbon的学习笔记和代码

---
+++ microservice-spring-cloud/microservice-consumer-movie-ribbon-properties-customizing/src/main/java/com/example/cloud/controller/MovieController.java
@@ -0,0 +1,37 @@
+package com.example.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.cloud.entity.User;
+
+@RestController
+public class MovieController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private LoadBalancerClient LoadBalancerClient;
+	
+	@RequestMapping("/movie/{id}")
+	public User findById(@PathVariable Long id) {
+//		return restTemplate.getForObject("http://localhost:7900/simple/"+id, User.class);
+		return restTemplate.getForObject("http://microservice-provider-user/simple/"+id, User.class);
+	}
+	
+	@GetMapping("/test")
+	public void test() {
+		ServiceInstance choose = LoadBalancerClient.choose("microservice-provider-user");
+		System.out.println("1111"+":"+choose.getPort()+";"+choose.getHost()+";"+choose.getServiceId());
+		ServiceInstance choose2 = LoadBalancerClient.choose("microservice-provider-user2");
+		System.out.println("2222"+":"+choose2.getPort()+";"+choose2.getHost()+";"+choose2.getServiceId());
+	}
+	
+}
--- microservice-spring-cloud/microservice-provider-user/src/main/java/com/example/cloud/controller/UserController.java
+++ microservice-spring-cloud/microservice-provider-user/src/main/java/com/example/cloud/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.example.cloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cloud.entity.User;
 import com.example.cloud.repository.UserRepository;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 public class UserController {
@@ -14,9 +18,26 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private EurekaClient eurekaClient;
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	
 	@GetMapping("/simple/{id}")
 	public User findById(@PathVariable Long id) {
 		return userRepository.findOne(id);
+	}
+	
+	@GetMapping("eureka-instance")
+	public String serviceUrl() {
+	    InstanceInfo instance = eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+	    return instance.getHomePageUrl();
+	}
+	@GetMapping("instance-info")
+	public ServiceInstance showInfo() {
+		ServiceInstance instance = discoveryClient.getLocalServiceInstance();
+		return instance;
 	}
 }
 
